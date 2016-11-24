bb46e6e4e7b82a3a558ed5d38cf2585ff13d8ac4
eda003 <james.brown9809@hotmail.co.nz>
2016-11-24 18:03:04 +1300

    showing qunatity in the cart

--- index.js
+++ index.js
@@ -13,20 +13,24 @@ const initialState = {
     {id: 0, name: 'Metallica', price: 10},
     {id: 1, name: 'Motorhead', price: 20}
   ],
-  cart: {0: 1}
+  cart: {
+    0: 2,
+    1: 0
+  }
 }
 
 var main = document.querySelector('main')
 
-const {dispatch, subscribe, getState} = createStore(reducer, initialState)
+const { dispatch, subscribe, getState } = createStore(reducer, initialState)
 
 
 subscribe(() => {
   const state = getState()
-  render( <Products
-    state = {state}
-     />
-     , main)
+  render(
+    <div>
+      <Products state = {state} />
+      <Cart state = {state} />
+    </div> , main)
 })
 
 
--- views/cart.js
+++ views/cart.js
@@ -1,10 +1,19 @@
 const React = require('react');
 
 
-const Cart = (props) => {
+const Cart = (props) =>
   <div>
-    <p>{props.cart}</p>
+    <h3>Here is the cart: </h3>
+    <ul>
+      {
+        Object.keys(props.state.cart).map((id) => {
+         return <li>{props.state.products[id].name}
+                     <p>Qty: {props.state.cart[id]}</p>
+                </li>
+      })}
+    </ul>
   </div>
-}
+
+
 
 module.exports = Cart
